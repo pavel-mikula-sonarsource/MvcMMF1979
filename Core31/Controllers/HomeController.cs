35e61483e428ee5112b882d460c5bc73ec6a356e
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-05-18 11:58:37 +0200

    Data propagation

--- Core31/Controllers/HomeController.cs
+++ Core31/Controllers/HomeController.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Core31.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Core31.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        public IActionResult Index(string value)
         {
             ViewBag.Title = "Home Page";
+            value = HttpContext.Request.Query["Key"];
+
+
+
+            ViewData["Value"] = "ViewData: " + value;
+            ViewBag.Value = "ViewBag: " + value;
+            HttpContext.Items["Value"] = "HttpContextItems: " + value;
+
             var model = new Models.Article("Title", "Text");
             return View(model);
         }
