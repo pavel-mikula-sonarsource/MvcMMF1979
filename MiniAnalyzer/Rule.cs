4cf29fce6198d2b55736c396d39139334e85535a
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-06-05 12:59:45 +0200

    Layout pages

--- MiniAnalyzer/Rule.cs
+++ MiniAnalyzer/Rule.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-//using Microsoft.AspNetCore.Razor;
-//using Microsoft.AspNetCore.Razor.Language;
-//using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
-//using Microsoft.AspNetCore.Razor.Language.Components;
-//using Microsoft.AspNetCore.Razor.Language.Extensions;
-//using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
 
 namespace MiniAnalyzer
 {
@@ -22,7 +16,7 @@ namespace MiniAnalyzer
     {
         public const string DiagnosticId = "XMini";
 
-        private string logFN;
+        private string fnPrefix;
 
         private static DiagnosticDescriptor rule = new DiagnosticDescriptor(DiagnosticId, "XXX", "{0}", "MMF-1979", DiagnosticSeverity.Warning, isEnabledByDefault: true);
 
@@ -30,9 +24,7 @@ namespace MiniAnalyzer
 
         public override void Initialize(AnalysisContext context)
         {
-            logFN = $@"C:\_Temp\Log {DateTime.Now.ToString("yyyy-MM-dd HHmmss")}.txt";
-            if (!File.Exists(logFN))
-                File.WriteAllText(logFN, "");
+            fnPrefix = $@"C:\_Temp\Log {DateTime.Now:yyyy-MM-dd HHmmss}";
             try
             {
                 context.RegisterCompilationStartAction(
@@ -43,36 +35,47 @@ namespace MiniAnalyzer
                             {
                                 try
                                 {
-                                    //if (c.Node.ToString().Contains(@"Views\Home\Index.cshtml") ||
-                                    //    c.Node.ToString().Contains(@"_Layout.cshtml") ||
-                                    //    c.Node.ToString().Contains(@"_ViewStart.cshtml"))
+                                    if (c.Node.ToString().Contains(@"Views\Home\Index.cshtml") ||
+                                        c.Node.ToString().Contains(@"_Layout.cshtml") ||
+                                        c.Node.ToString().Contains(@"_ViewStart.cshtml")
+                                        && !c.Node.ToString().Contains("PrecompiledSample"))
                                     {
-                                        //throw new System.Exception("Test");
                                         var msg = c.SemanticModel.GetDeclaredSymbol(c.Node as MethodDeclarationSyntax).ToMinimalDisplayString(c.SemanticModel, c.Node.GetLocation().SourceSpan.Start);
-                                        Log("All: " + msg);
-                                        //Log(Environment.StackTrace);
-                                        Log(c.Node.SyntaxTree.ToString());
+                                        Log(c.Node.SyntaxTree.ToString(), msg);
 
-                                        var diagnostic = Diagnostic.Create(rule, c.Node.GetLocation(), msg);
-                                        c.ReportDiagnostic(diagnostic);
+                                        //var diagnostic = Diagnostic.Create(rule, c.Node.GetLocation(), msg);
+                                        //c.ReportDiagnostic(diagnostic);
                                     }
                                 }
                                 catch (Exception ex)
                                 {
-                                    Log($"Error {ex.GetType().Name}: {ex.Message}\n\n{ex.ToString()}");
+                                    Log($"Error {ex.GetType().Name}: {ex.Message}\n\n{ex}", "Error");
                                 }
                             }, SyntaxKind.MethodDeclaration);
                     });
             }
             catch (Exception ex)
             {
-                Log($"Main Error {ex.GetType().Name}: {ex.Message}\n\n{ex.ToString()}");
+                Log($"Main Error {ex.GetType().Name}: {ex.Message}\n\n{ex.ToString()}", "Error");
             }
         }
 
-        private void Log(string msg)
+        private void Log(string msg, string fnInfix)
         {
-            File.AppendAllText(logFN, msg + "\n");
+            foreach(var c in Path.GetInvalidFileNameChars())
+            {
+                fnInfix = fnInfix.Replace(c, '-');
+            }
+            for (var i = 0; i < 100; i++)
+            {
+                var fn = $"{fnPrefix} {fnInfix} {i:00}.txt";
+                if (!File.Exists(fn))
+                {
+                    File.WriteAllText(fn, msg);
+                    return;
+                }
+
+            }
         }
     }
 }
