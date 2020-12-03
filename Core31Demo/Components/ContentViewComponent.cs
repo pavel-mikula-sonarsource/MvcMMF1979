406a21012e7121d87c63d65baf1423894b9e7c64
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-12-03 12:05:15 +0100

    Core31Demo

---
+++ Core31Demo/Components/ContentViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core31.Components
+{
+    public class ContentViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Core31Demo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core31.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string page, string message)
+        {
+            if(page == null)
+            {
+                ViewBag.Title = "Welcome";
+                ViewBag.Content = "This is a great page!";
+                return View();
+            }
+
+            ViewData["Title"] = "Page " + page + " not found";
+            ViewData["Content"] = message;
+            return View();
+        }
+    }
+}
